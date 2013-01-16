118876820abd97a659afd888e1bad7710a3b2137
Tim Uruski <timuruski@gmail.com>
2013-01-15 22:19:52 -0700

    Add script to fetch feed XML

---
+++ script/fetch-feeds.rb
@@ -0,0 +1,22 @@
+require 'open-uri'
+
+Feed.find_each do |f|
+  filename = f.title.parameterize
+  file_path = "tmp/feeds/#{filename}.xml"
+  next if File.exists?(file_path)
+
+  url = URI.parse(f.feed_url)
+
+  begin
+    open(url) do |xml|
+      system('cp', xml.path, file_path)
+      puts "Fetched #{file_path}"
+    end
+  rescue => e
+    puts "Could not fetch #{f.title}"
+    puts "  #{f.feed_url}"
+    puts "  #{e.message}"
+  else
+    # File.write(file_path, xml.read)
+  end
+end
