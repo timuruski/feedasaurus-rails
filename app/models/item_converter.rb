2736572a32873740ae67b169d159d4ddd78dd412
Tim Uruski <timuruski@gmail.com>
2013-01-20 11:51:46 -0700

    Break ItemConverter into specific types

--- app/models/item_converter.rb
+++ app/models/item_converter.rb
@@ -1,43 +1,54 @@
 class ItemConverter < Struct.new(:item)
-  def convert
-    case item
-    when RSS::Rss::Channel::Item then convert_rss(item)
-    when RSS::Atom::Feed::Entry then convert_atom(item)
-    when RSS::RDF::Item then convert_rdf(item)
+  def self.convert(item)
+    converter_class = case item
+    when RSS::Atom::Feed::Entry then AtomItemConverter
+    when RSS::RDF::Item then RDFItemConverter
+    when RSS::Rss::Channel::Item then RSSItemConverter
     end
-  end
 
-  def convert_rss(item)
-    Item.new do |i|
-      i.url = convert_link(item.link)
-      i.title = item.title
-      # i.author = item.author
-      # i.content = item.content
-      i.published_at = Time.current
-    end
-  end
-
-  def convert_atom(item)
-    Item.new do |i|
-      i.url = convert_link(item.link)
-      i.title = item.title.content
-      i.author = item.author.content
-      i.content = item.content
-      i.published_at = item.published
-    end
-  end
-
-  def convert_rdf(item)
-    Item.new do |i|
-      i.url = convert_link(item.link)
-      i.title = item.title
-      # i.author = item.author
-      # i.content = item.content
-      i.published_at = Time.current
-    end
+    converter_class.new(item).convert
   end
 
   def convert_link(link)
     link.respond_to?(:href) ? link.href : link.to_s
+  end
+
+  def try_content
+  end
+end
+
+class AtomItemConverter < ItemConverter
+  def convert
+    Item.new do |i|
+      i.url = item.link.try(:href)
+      i.title = item.title.try(:content)
+      i.author = item.author.try(:name).try(:content)
+      i.content = item.content.try(:content)
+      i.published_at = item.published.try(:content)
+    end
+  end
+end
+
+class RSSItemConverter < ItemConverter
+  def convert
+    Item.new do |i|
+      i.url = item.link
+      i.title = item.title
+      i.author = item.author
+      i.content = item.description
+      i.published_at = item.pubDate
+    end
+  end
+end
+
+class RDFItemConverter < ItemConverter
+  def convert
+    Item.new do |i|
+      i.url = item.link
+      i.title = item.title
+      i.author = item.dc_creator
+      i.content = item.description
+      i.published_at = item.dc_date
+    end
   end
 end
